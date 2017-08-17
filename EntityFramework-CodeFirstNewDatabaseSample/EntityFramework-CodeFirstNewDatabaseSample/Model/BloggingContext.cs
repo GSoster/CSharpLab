7514c97b76c850f1432aff532a73d0f5087c8526
GSoster <guilhermesosters@gmail.com>
2017-08-16 21:24:18 -0300

    EntityFramework-CodeFirst: added class BloggingContext:DbContext

---
+++ EntityFramework-CodeFirstNewDatabaseSample/EntityFramework-CodeFirstNewDatabaseSample/Model/BloggingContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+namespace EntityFramework_CodeFirstNewDatabaseSample.Model
+{
+    class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
