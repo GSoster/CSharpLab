c9a3a81e3e763e3ee1dd4f741ab37c18f9f85b39
GSoster <guilhermesosters@gmail.com>
2017-08-17 10:26:44 -0300

    EntityFramework-CodeFirst: added migration and updated the database

---
+++ EntityFramework-CodeFirstNewDatabaseSample/EntityFramework-CodeFirstNewDatabaseSample/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace EntityFramework_CodeFirstNewDatabaseSample.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EntityFramework_CodeFirstNewDatabaseSample.BloggingContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(EntityFramework_CodeFirstNewDatabaseSample.BloggingContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
