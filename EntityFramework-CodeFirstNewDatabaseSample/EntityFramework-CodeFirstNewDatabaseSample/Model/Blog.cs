8039ca0c79655a4a3e68b5fa0310709e302de952
GSoster <guilhermesosters@gmail.com>
2017-08-16 21:12:16 -0300

    EntityFramework-CodeFirst: added new Model Class Blog

---
+++ EntityFramework-CodeFirstNewDatabaseSample/EntityFramework-CodeFirstNewDatabaseSample/Model/Blog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework_CodeFirstNewDatabaseSample
+{
+    class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<Post> Posts { get; set; }
+    }
+}
