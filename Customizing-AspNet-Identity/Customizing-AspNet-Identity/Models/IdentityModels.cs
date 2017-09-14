f39d7b3b40f0ca50309afba2bf2f7fc682f9e33e
Guilherme Soster Santos <GSOSTER@senacrs.com.br>
2017-09-14 16:27:22 -0300

    AspNET MVC Identity: modified ApplicationUser class to include Name, Lastname and E-mail

--- Customizing-AspNet-Identity/Customizing-AspNet-Identity/Models/IdentityModels.cs
+++ Customizing-AspNet-Identity/Customizing-AspNet-Identity/Models/IdentityModels.cs
@@ -9,6 +9,10 @@ namespace Customizing_AspNet_Identity.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
