9479b8a4d4d5a353991b5cbc59463a5b5f118547
Guilherme Soster Santos <GSOSTER@senacrs.com.br>
2017-09-14 16:24:26 -0300

    Added new project to learn how to work with ASPNET Identity. A tool to have user management

---
+++ Customizing-AspNet-Identity/Customizing-AspNet-Identity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Customizing_AspNet_Identity.MvcApplication" Language="C#" %>
---
+++ Customizing-AspNet-Identity/Customizing-AspNet-Identity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Customizing_AspNet_Identity.Startup))]
+namespace Customizing_AspNet_Identity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
