20fab5007c07f430a8b02f5a38563cb633e260cc
GSoster <guilhermesosters@gmail.com>
2017-09-17 22:31:14 -0300

    PhotoGallery: Added Photo class to represent a single image

--- PhotoGallery/PhotoGallery/Photo.cs
+++ PhotoGallery/PhotoGallery/Photo.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace PhotoGallery
 {
-    class Photo
+    /// <summary>
+    ///     This class describes a single photo - its location, the image.
+    /// </summary>
+    public class Photo
     {
+        private readonly Uri _source;
+
+        public string Source { get; }
+        public BitmapFrame Image { get; set; }
+
+
+
+        public Photo(string path)
+        {
+            Source = path;
+            _source = new Uri(path);
+            Image = BitmapFrame.Create(_source);
+        }
+
+        public override string ToString()
+        {
+            return _source.ToString();
+        }
+
     }
 }
