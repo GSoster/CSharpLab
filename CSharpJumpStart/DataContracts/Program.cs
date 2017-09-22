ca70d98c30614583e1342a88ee850a135c53a3ab
GSoster <guilhermesosters@gmail.com>
2017-09-21 22:56:51 -0300

    CSharp Jump Start: Code Contracts

---
+++ CSharpJumpStart/DataContracts/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContracts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public abstract class Animal
+        {
+            public string Name { get; protected set; }
+            public abstract void SetName(string value);
+        }
+
+        public class Cat : Animal
+        {
+            public override void SetName(string value)
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException("value");
+
+                if (value == this.Name)
+                    throw new ArgumentException("value is duplicate");
+
+                this.Name = value;
+            }
+
+        }
+
+        public class Dog : Animal
+        {
+            public override void SetName(string value)
+            {
+                //code contract - validade input
+                Contract.Requires(!string.IsNullOrWhiteSpace("value is empty"));
+                this.Name = value;
+            }
+
+            public string GetName()
+            {
+                //validade output
+                Contract.Ensures(!string.IsNullOrWhiteSpace(Contract.Result<string>()));
+                return this.Name;
+            }
+        }
+    }
+}
