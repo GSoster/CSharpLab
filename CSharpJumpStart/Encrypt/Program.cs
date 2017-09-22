9641d5a866f7fe571e765836231810538d554b86
GSoster <guilhermesosters@gmail.com>
2017-09-21 22:15:06 -0300

    CSharp Jump Start: Added code for Windows Protection Data  Encrypt

--- CSharpJumpStart/Encrypt/Program.cs
+++ CSharpJumpStart/Encrypt/Program.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Security.Cryptography;
+
 namespace Encrypt
 {
     class Program
@@ -30,6 +32,26 @@ namespace Encrypt
             File.Encrypt(fileName);
 
             #endregion
+
+            /*
+             * Windows Data Protection
+             * It uses the capabilities of the Windows operating system
+             * - Encrypts and decrypts byte[]
+             *  - Fast to encrypt/decrypt
+             *  - Based on our credentials
+             */
+            #region Windows Data Protection
+
+            //null can be replaced with a byte[] for aditional entropy
+            //DataProtectionScope.CurrentUser could also be DataProtectionScope.CurrentMachine
+            var wdpEncryptedData = ProtectedData.Protect(dataToProtectArray, null, DataProtectionScope.CurrentUser);
+
+            var wdpnUnEncryptedData = ProtectedData.Unprotect(wdpEncryptedData, null, DataProtectionScope.CurrentUser);
+            var wdpnUnEncryptedString = Encoding.Unicode.GetString(wdpnUnEncryptedData);
+
+
+            #endregion
+
         }
     }
 }
