b2e08fbe2fcbf0b3b7c377240d344668a33e92de
GSoster <guilhermesosters@gmail.com>
2017-09-21 22:03:17 -0300

    CSharp Jump Start: Added code for simple Encrypt

--- CSharpJumpStart/Encrypt/Program.cs
+++ CSharpJumpStart/Encrypt/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace Encrypt
     {
         static void Main(string[] args)
         {
+            const string dataToProtect = "This is a bunch of super secret content!";
+            var dataToProtectArray = Encoding.Unicode.GetBytes(dataToProtect);
+
+            /* Simple file encryption
+             *  - Encrypts and Decrypts Files
+             *  - Fast to encrypt/decrypt
+             *  - Based on our credentials
+             */
+            #region File.Encrypt
+
+            var fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "MyDataFile.txt");
+
+            //encrypt a file in the file system
+            File.WriteAllText(fileName, dataToProtect);
+
+            //now we can encrypt it - only we can acess it now
+            File.Encrypt(fileName);
+
+            #endregion
         }
     }
 }
