218afedc46ded58b7c747ce5d4d096f6aa672f1d
GSoster <guilhermesosters@gmail.com>
2017-10-01 21:40:32 -0300

    PhotoGallery: Added Tag class

---
+++ PhotoGallery/PhotoGallery/Model/Tag.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoGallery.Model
+{
+    class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }//the name displayed to user
+        public string Description { get; set; }
+        string _InternalName; // it is used to sort/filter should always be lowercase
+        public string InternalName {
+            get { return _InternalName.ToLower(); }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException();
+                _InternalName = value.ToLower();
+            }
+        }
+    }
+}
