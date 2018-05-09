dec56bb311481d7100c7a1a68f0561506ad53c8d
Guilherme Soster <GSoster@users.noreply.github.com>
2018-05-09 16:42:09 -0300

    added logic to handle 2 players

--- WindowsForms-Tic-tac-toe/WindowsForms-Tic-tac-toe/Form1.Designer.cs
+++ WindowsForms-Tic-tac-toe/WindowsForms-Tic-tac-toe/Form1.Designer.cs
@@ -37,85 +37,98 @@
             this.btnBottomRight = new System.Windows.Forms.Button();
             this.btnBottomCenter = new System.Windows.Forms.Button();
             this.btnBottomLeft = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnTopLeft
             // 
-            this.btnTopLeft.Location = new System.Drawing.Point(12, 12);
+            this.btnTopLeft.Location = new System.Drawing.Point(11, 44);
             this.btnTopLeft.Name = "btnTopLeft";
-            this.btnTopLeft.Size = new System.Drawing.Size(79, 72);
+            this.btnTopLeft.Size = new System.Drawing.Size(85, 72);
             this.btnTopLeft.TabIndex = 0;
             this.btnTopLeft.UseVisualStyleBackColor = true;
+            this.btnTopLeft.Click += new System.EventHandler(this.btnTopLeft_Click);
             // 
             // btnTopCenter
             // 
-            this.btnTopCenter.Location = new System.Drawing.Point(115, 12);
+            this.btnTopCenter.Location = new System.Drawing.Point(114, 44);
             this.btnTopCenter.Name = "btnTopCenter";
-            this.btnTopCenter.Size = new System.Drawing.Size(79, 72);
+            this.btnTopCenter.Size = new System.Drawing.Size(85, 72);
             this.btnTopCenter.TabIndex = 1;
             this.btnTopCenter.UseVisualStyleBackColor = true;
+            this.btnTopCenter.Click += new System.EventHandler(this.btnTopCenter_Click);
             // 
             // btnTopRight
             // 
-            this.btnTopRight.Location = new System.Drawing.Point(221, 12);
+            this.btnTopRight.Location = new System.Drawing.Point(220, 44);
             this.btnTopRight.Name = "btnTopRight";
-            this.btnTopRight.Size = new System.Drawing.Size(79, 72);
+            this.btnTopRight.Size = new System.Drawing.Size(85, 72);
             this.btnTopRight.TabIndex = 2;
             this.btnTopRight.UseVisualStyleBackColor = true;
             // 
             // btnMiddleRight
             // 
-            this.btnMiddleRight.Location = new System.Drawing.Point(221, 90);
+            this.btnMiddleRight.Location = new System.Drawing.Point(220, 122);
             this.btnMiddleRight.Name = "btnMiddleRight";
-            this.btnMiddleRight.Size = new System.Drawing.Size(79, 72);
+            this.btnMiddleRight.Size = new System.Drawing.Size(85, 72);
             this.btnMiddleRight.TabIndex = 5;
             this.btnMiddleRight.UseVisualStyleBackColor = true;
             // 
             // btnMiddleCenter
             // 
-            this.btnMiddleCenter.Location = new System.Drawing.Point(115, 90);
+            this.btnMiddleCenter.Location = new System.Drawing.Point(114, 122);
             this.btnMiddleCenter.Name = "btnMiddleCenter";
-            this.btnMiddleCenter.Size = new System.Drawing.Size(79, 72);
+            this.btnMiddleCenter.Size = new System.Drawing.Size(85, 72);
             this.btnMiddleCenter.TabIndex = 4;
             this.btnMiddleCenter.UseVisualStyleBackColor = true;
             // 
             // btnMiddleLeft
             // 
-            this.btnMiddleLeft.Location = new System.Drawing.Point(12, 90);
+            this.btnMiddleLeft.Location = new System.Drawing.Point(11, 122);
             this.btnMiddleLeft.Name = "btnMiddleLeft";
-            this.btnMiddleLeft.Size = new System.Drawing.Size(79, 72);
+            this.btnMiddleLeft.Size = new System.Drawing.Size(85, 72);
             this.btnMiddleLeft.TabIndex = 3;
             this.btnMiddleLeft.UseVisualStyleBackColor = true;
             // 
             // btnBottomRight
             // 
-            this.btnBottomRight.Location = new System.Drawing.Point(221, 168);
+            this.btnBottomRight.Location = new System.Drawing.Point(220, 200);
             this.btnBottomRight.Name = "btnBottomRight";
-            this.btnBottomRight.Size = new System.Drawing.Size(79, 72);
+            this.btnBottomRight.Size = new System.Drawing.Size(85, 72);
             this.btnBottomRight.TabIndex = 8;
             this.btnBottomRight.UseVisualStyleBackColor = true;
             // 
             // btnBottomCenter
             // 
-            this.btnBottomCenter.Location = new System.Drawing.Point(115, 168);
+            this.btnBottomCenter.Location = new System.Drawing.Point(114, 200);
             this.btnBottomCenter.Name = "btnBottomCenter";
-            this.btnBottomCenter.Size = new System.Drawing.Size(79, 72);
+            this.btnBottomCenter.Size = new System.Drawing.Size(85, 72);
             this.btnBottomCenter.TabIndex = 7;
             this.btnBottomCenter.UseVisualStyleBackColor = true;
             // 
             // btnBottomLeft
             // 
-            this.btnBottomLeft.Location = new System.Drawing.Point(12, 168);
+            this.btnBottomLeft.Location = new System.Drawing.Point(11, 200);
             this.btnBottomLeft.Name = "btnBottomLeft";
-            this.btnBottomLeft.Size = new System.Drawing.Size(79, 72);
+            this.btnBottomLeft.Size = new System.Drawing.Size(85, 72);
             this.btnBottomLeft.TabIndex = 6;
             this.btnBottomLeft.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Turn: ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(315, 252);
+            this.ClientSize = new System.Drawing.Size(321, 284);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnBottomRight);
             this.Controls.Add(this.btnBottomCenter);
             this.Controls.Add(this.btnBottomLeft);
@@ -128,6 +141,7 @@
             this.Name = "Form1";
             this.Text = "Tic-tac-toe";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -142,6 +156,7 @@
         private System.Windows.Forms.Button btnBottomRight;
         private System.Windows.Forms.Button btnBottomCenter;
         private System.Windows.Forms.Button btnBottomLeft;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WindowsForms-Tic-tac-toe/WindowsForms-Tic-tac-toe/Form1.cs
+++ WindowsForms-Tic-tac-toe/WindowsForms-Tic-tac-toe/Form1.cs
@@ -12,9 +12,38 @@ namespace WindowsForms_Tic_tac_toe
 {
     public partial class Form1 : Form
     {
+
+        public bool isPlayerTurn = true; // player plays as X
+        public int[,,] grid = new int [3,3,3];
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnTopLeft_Click(object sender, EventArgs e)
+        {
+            if (CheckPlacement(sender))
+            {
+                if (isPlayerTurn)
+                    btnTopLeft.Text = "X";
+                else
+                    btnTopLeft.Text = "O";
+                isPlayerTurn = !isPlayerTurn;
+            }
+            
+                
+
+        }
+
+        private bool CheckPlacement (object btn)
+        {
+            return ((Button)btn).Text == string.Empty; ;
+        }
+
+        private void btnTopCenter_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
