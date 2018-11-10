96d1d2b8b050f6ab32aacc320f298b59cba05dc3
Guilherme Soster <GSoster@users.noreply.github.com>
2018-11-10 14:20:15 -0200

    Starting to work with the Models

---
+++ CSharpRPG/Engine/ViewModels/GameSession.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Engine.Models;
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        public Player CurrentPlayer{ get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player(){Name = "Teste Player", Gold = 1000};
+            
+        }
+    }
+}
