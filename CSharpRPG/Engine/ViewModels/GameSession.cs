65366bdf760bf24a60846f748ae7e158ffd1a406
Guilherme Soster <GSoster@users.noreply.github.com>
2018-11-14 00:43:59 -0200

    Now player can move in the world!

--- CSharpRPG/Engine/ViewModels/GameSession.cs
+++ CSharpRPG/Engine/ViewModels/GameSession.cs
@@ -3,14 +3,60 @@ using System.Collections.Generic;
 using System.Text;
 using Engine.Models;
 using Engine.Factories;
+using System.ComponentModel;
 
 namespace Engine.ViewModels
 {
-    public class GameSession
+    public class GameSession : INotifyPropertyChanged
     {
+
+        private Location _currentLocation;
+
         public World CurrentWorld { get; set; }
         public Player CurrentPlayer{ get; set; }
-        public Location CurrentLocation { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Location CurrentLocation {
+            get {return _currentLocation;}
+            set {
+                _currentLocation = value;
+                OnPropertyChanged("CurrentLocation");
+                OnPropertyChanged("HasLocationToNorth");
+                OnPropertyChanged("HasLocationToSouth");
+                OnPropertyChanged("HasLocationToEast");
+                OnPropertyChanged("HasLocationToWest");
+            }
+        }
+
+        public bool HasLocationToNorth {
+            get {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1) != null;
+            }
+        }
+
+        public bool HasLocationToSouth
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1) != null;
+            }
+        }
+
+        public bool HasLocationToEast
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate) != null;
+            }
+        }
+
+        public bool HasLocationToWest
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate) != null;
+            }
+        }
 
         public GameSession()
         {
@@ -29,5 +75,36 @@ namespace Engine.ViewModels
             CurrentWorld = wFactory.CreateWorld();
             CurrentLocation = CurrentWorld.LocationAt(0, 0);
         }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+        }
+
+        public void MoveNorth()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1);
+        }
+
+        public void MoveSouth()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
+        }
+
+        public void MoveWest()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
+        }
+
+        public void MoveEast()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate);
+        }
+
     }
 }
