fa4aa3cbb4f2465221f34b730a8efcbdebc7c31c
Guilherme Soster <GSoster@users.noreply.github.com>
2018-11-16 09:51:42 -0200

    Implemented GameItem and Weapon class and the GameItemFactory

---
+++ CSharpRPG/Engine/Models/GameItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class GameItem
+    {
+        public int ItemTypeID { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public GameItem(int itemTypeId, string name, int price)
+        {
+            ItemTypeID = itemTypeId;
+            Name = name;
+            Price = price;
+        }
+
+        public GameItem Clone()
+        {
+            return new GameItem(ItemTypeID, Name, Price);
+        }
+
+    }
+}
---
+++ CSharpRPG/Engine/Models/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Weapon : GameItem
+    {
+        public int MinimumDamage { get; set; }
+        public int MaximumDamage { get; set; }
+
+        public Weapon(int itemTypeId, string name, int price, int minDamage, int maxDamage)
+            :base(itemTypeId, name, price)
+        {
+            MinimumDamage = minDamage;
+            MaximumDamage = maxDamage;
+        }
+
+        // the 'new' keyword in the method signature is to identify that this implementation hides the one from GameItem
+        public new Weapon Clone()
+        {
+            return new Weapon(ItemTypeID, Name, Price, MinimumDamage, MaximumDamage);
+        }
+
+    }
+}
