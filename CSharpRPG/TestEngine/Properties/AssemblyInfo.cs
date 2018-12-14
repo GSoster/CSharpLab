5ee63a778bcce3ab0e11d777747fe727f9935acf
Guilherme Soster <GSoster@users.noreply.github.com>
2018-12-14 14:31:24 -0200

    Added test proejct to the engine

---
+++ CSharpRPG/TestEngine/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestEngine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestEngine")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a5385be0-4598-400f-a8cb-b0b8e18e55e1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CSharpRPG/TestEngine/ViewModels/TestGameSession.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Engine.ViewModels;
+namespace TestEngine.ViewModels
+{
+    [TestClass]
+    public class TestGameSession
+    {
+        [TestMethod]
+        public void TestCreateGameSession()
+        {
+            GameSession gameSession = new GameSession();
+            Assert.IsNotNull(gameSession.CurrentPlayer);
+            Assert.IsNotNull(gameSession.CurrentLocation);
+            Assert.AreEqual("Town square", gameSession.CurrentLocation.Name);
+        }
+
+        [TestMethod]
+        public void TestPlayerMovesHomeAndIsCompletelyHealedOnKilled()
+        {
+            //setup
+            GameSession gameSession = new GameSession();
+            gameSession.CurrentPlayer.TakeDamage(999);
+
+            Assert.Equals("Home", gameSession.CurrentLocation.Name);
+            //TODO: this number 10 should be some sort of constant somewhere (player class?)
+            Assert.Equals(gameSession.CurrentPlayer.Level * 10, gameSession.CurrentPlayer.CurrentHitPoints);
+        }
+    }
+}
