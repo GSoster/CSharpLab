c82c83801a50f99fd12f557cad2490b8329f1c8b
Santos <Guilherme_Santos3@Dell.com>
2019-03-19 16:06:28 -0300

    Added Hook class to allow the use of custom code before or after scenarios, features, steps, etc

---
+++ Business Readable Automated Tests with SpecFlow/GameCore.Specs/Hooks.cs
@@ -0,0 +1,24 @@
+﻿using TechTalk.SpecFlow;
+
+namespace GameCore.Specs
+{
+    [Binding]
+    ///
+    /// We inherit from Steps so it is possible to use the 
+    /// ScenarioInfo in a thread safe way by the use of the "THIS" keyword
+    public class Hooks : Steps
+    {
+
+        [BeforeScenario("elf")]//will only execute before scenarios with tag elf
+        public void BeforeScenario()
+        {
+
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+
+        }
+    }
+}
