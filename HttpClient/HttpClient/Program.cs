b32e3db7ccd7b2fb1512a77c464c5f8b34f90660
Santos <Guilherme_Santos3@Dell.com>
2019-03-20 17:17:26 -0300

    Added project to interact with APIs using HttpClient

---
+++ HttpClient/HttpClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpClientAPIConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("...");
+            HttpClientHandler handler = new HttpClientHandler();            
+            WebProxy proxy = new WebProxy("http://proxy.us.dell.com:80");
+            proxy.UseDefaultCredentials = true;            
+            handler.Proxy = proxy;
+            HttpClient client = new HttpClient(handler);
+            string uri = "https://jsonplaceholder.typicode.com/posts";
+            var response = client.GetStringAsync(uri).GetAwaiter().GetResult();
+            Console.WriteLine(response);
+            
+        }
+        
+    }
+}
