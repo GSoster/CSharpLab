3bdfd64b7825c5868952df3d73634738a4268722
Santos <Guilherme_Santos3@Dell.com>
2019-03-21 17:47:10 -0300

    Added test project to XUnit study

---
+++ xunitAndDotNetCore/GameEngine.Tests/PlayerCharacterShould.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class PlayerCharacterShould
+    {
+        [Fact]
+        public void BeInexperiencedWhenNew()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+        }
+    }
+}
