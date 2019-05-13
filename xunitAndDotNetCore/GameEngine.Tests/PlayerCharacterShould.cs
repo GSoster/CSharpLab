a8e11c45c13342acdd657e9b9704bf82e33d8b29
Guilherme Soster Santos <Guilherme_Santos3@Dell.com>
2019-05-13 14:24:33 -0300

    added code for courses about tests

--- xunitAndDotNetCore/GameEngine.Tests/PlayerCharacterShould.cs
+++ xunitAndDotNetCore/GameEngine.Tests/PlayerCharacterShould.cs
@@ -9,6 +9,55 @@ namespace GameEngine.Tests
         public void BeInexperiencedWhenNew()
         {
             PlayerCharacter sut = new PlayerCharacter();
+            Assert.True(sut.IsNoob);
         }
+
+        [Fact]
+        public void CalculateFullName()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+            sut.FirstName = "Guilherme";
+            sut.LastName = "Soster";
+
+            Assert.Equal("Guilherme Soster",sut.FullName);
+        }
+
+        [Fact]
+        public void HaveFulllNameStartingWithFirstName()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+            sut.FirstName = "Guilherme";
+            sut.LastName = "Soster";
+            Assert.StartsWith("Guilherme", sut.FullName);
+        }
+
+        [Fact]
+        public void HaveFulllNameEndingWithLastName()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+            sut.FirstName = "Guilherme";
+            sut.LastName = "Soster";
+            Assert.EndsWith("Soster", sut.FullName);
+        }
+
+        [Fact]
+        public void CalculateFullName_IgnoreCase()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+            sut.FirstName = "Guilherme";
+            sut.LastName = "Soster";
+            Assert.Equal("Guilherme Soster", sut.FullName, ignoreCase: true);
+        }
+
+        [Fact]
+        public void CalculateFullName_Substring()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+            sut.FirstName = "Guilherme";
+            sut.LastName = "Soster";
+            Assert.Contains("me Sos", sut.FullName);
+        }
+
+
     }
 }
