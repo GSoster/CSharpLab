f499efbc047a049b6ee6c8989f3522c786ffabb8
Guilherme Soster <guilhermesosters@gmail.com>
2021-07-16 13:46:31 -0300

    Add implementation on ContosoPizza controller

--- ContosoPizza/Controllers/PizzaController.cs
+++ ContosoPizza/Controllers/PizzaController.cs
@@ -18,11 +18,51 @@ namespace ContosoPizza.Controllers
         public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
 
         // GET by Id action
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> GetPizza(int id) {
+            var pizza = PizzaService.Get(id);  
+            if (pizza is null)
+                return NotFound();
+
+            return pizza; //Ok() is implied
+        } 
 
         // POST action
+        [HttpPost]
+        //Because the controller is annotated with the [ApiController] attribute, it's implied that the Pizza parameter will be found in the request body.
+        public ActionResult CreatePizza(Pizza pizza) {
+          PizzaService.Add(pizza);          
+          return CreatedAtAction(nameof(CreatePizza), new {Id = pizza.Id}, pizza);  
+        } 
 
         // PUT action
+        [HttpPut("{id}")]
+        //Returns IActionResult because the ActionResult return type isn't known until runtime.
+        public IActionResult UpdatePizza(int id, Pizza pizza)
+        {
+            if (id != pizza.Id)
+                return BadRequest();
+            
+            var existingPizza = PizzaService.Get(id);
+            if (existingPizza is null)
+                return NotFound();
+
+            PizzaService.Update(pizza);
+            return NoContent();
+        }
+
 
         // DELETE action
+        [HttpDelete("{id}")]
+        //Returns IActionResult because the ActionResult return type isn't known until runtime.
+        public IActionResult DeletePizza(int id) 
+        {
+            var pizza = PizzaService.Get(id);
+            if (pizza is null)
+                return NotFound();
+            
+            PizzaService.Delete(id);
+            return NoContent();
+        }
     }
 }
